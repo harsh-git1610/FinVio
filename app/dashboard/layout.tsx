ffed824f86c138fc6e71497f17f83e74d01cbc3e
harsh-git1610 <w.harsh2005@gmail.com>
2026-02-16 22:55:46 +0530

    sticky

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -84,7 +84,7 @@ export default async function DashboardLayout({
         <SidebarProvider>
             <Sidebar userData={userData} />
             <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-neutral-200 dark:border-neutral-800">
+                <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 border-neutral-200 dark:border-neutral-800">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
