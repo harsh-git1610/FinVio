91bb35b44bdecc0a9dfc512bb01264ba3a82dba1
harsh-git1610 <w.harsh2005@gmail.com>
2026-02-17 23:51:06 +0530

    ok

--- app/dashboard/invoices/pdf/generatePdf.ts
+++ app/dashboard/invoices/pdf/generatePdf.ts
@@ -1,9 +1,24 @@
-import puppeteer from "puppeteer"
 import { getInvoiceTemplate } from "../templates/templates"
 import { Invoice } from "@prisma/client"
 
 export async function generateInvoicePdf(invoice: Invoice) {
-  const browser = await puppeteer.launch()
+  let browser
+
+  if (process.env.NODE_ENV === "production" || process.env.VERCEL) {
+    const chromium = await import("@sparticuz/chromium")
+    const puppeteer = await import("puppeteer-core")
+
+    browser = await puppeteer.default.launch({
+      args: chromium.default.args,
+      defaultViewport: chromium.default.defaultViewport,
+      executablePath: await chromium.default.executablePath(),
+      headless: chromium.default.headless,
+    })
+  } else {
+    const puppeteer = await import("puppeteer")
+    browser = await puppeteer.default.launch()
+  }
+
   const page = await browser.newPage()
 
   const html = getInvoiceTemplate(invoice)
